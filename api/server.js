e8e79519ec4e7c11ffb6043e635632415382283c
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-02 15:30:42 -0500

    add user

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const server = express()
+server.use(express.json())
 const User = require('./users/model')
 
 server.get('/api/users', (req, res) => {
@@ -29,6 +30,25 @@ server.get('/api/users/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({
                 message: 'error getting user',
+                err: err.message
+            })
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    }
+    User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error creating user',
                 err: err.message
             })
         })
