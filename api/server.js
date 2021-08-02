f4a02f8c0ad1315f1ed17cd7fe14aa316315f4e5
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-02 15:18:39 -0500

    check user id

--- api/server.js
+++ api/server.js
@@ -1,6 +1,38 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const server = express()
+const User = require('./users/model')
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: `{ message: "The user with the specified ID does not exist" }`
+                })
+            }
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting user',
+                err: err.message
+            })
+        })
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
