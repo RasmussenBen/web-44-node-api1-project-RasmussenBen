d3c1046bb303bbd9747652bc90c85fa20c877965
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-02 15:54:42 -0500

    testing

--- api/server.js
+++ api/server.js
@@ -39,7 +39,7 @@ server.post('/api/users', (req, res) => {
     const user = req.body
     if (!user.name || !user.bio) {
         res.status(400).json({
-            message: 'Please provide name and bio for the user'
+            message: "Please provide name and bio for the user"
         })
     }
     User.insert(user)
